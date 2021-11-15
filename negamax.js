06b6d4b7ddec37250943827399e5881935c65334
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2021-11-14 22:37:04 -0300

    feat: add negamax

---
+++ negamax.js
@@ -0,0 +1,63 @@
+const currentBoardState = ["X", 1, "O", 3, 4, 5, "O", 7, "X"];
+const aiMark = "X";
+const humanMark = "O";
+
+function getAllEmptyCellsIndexes(currBdSt) {
+    return currBdSt.filter(i => i !== "O" && i !== "X");
+}
+
+function checkIfWinnerFound(currBdSt, currMark) {
+    return (currBdSt[0] === currMark && currBdSt[1] === currMark && currBdSt[2] === currMark) ||
+        (currBdSt[3] === currMark && currBdSt[4] === currMark && currBdSt[5] === currMark) ||
+        (currBdSt[6] === currMark && currBdSt[7] === currMark && currBdSt[8] === currMark) ||
+        (currBdSt[0] === currMark && currBdSt[3] === currMark && currBdSt[6] === currMark) ||
+        (currBdSt[1] === currMark && currBdSt[4] === currMark && currBdSt[7] === currMark) ||
+        (currBdSt[2] === currMark && currBdSt[5] === currMark && currBdSt[8] === currMark) ||
+        (currBdSt[0] === currMark && currBdSt[4] === currMark && currBdSt[8] === currMark) ||
+        (currBdSt[2] === currMark && currBdSt[4] === currMark && currBdSt[6] === currMark);
+}
+
+function negamax(currBdSt, currMark) {
+    const availCellsIndexes = getAllEmptyCellsIndexes(currBdSt);
+
+    if (checkIfWinnerFound(currBdSt, humanMark)) {
+        return {score: -1};
+    } else if (checkIfWinnerFound(currBdSt, aiMark)) {
+        return {score: 1};
+    } else if (availCellsIndexes.length === 0) {
+        return {score: 0};
+    }
+
+    const allTestPlayInfos = [];
+
+    let bestTestPlay = null;
+    let bestScore = -Infinity;
+
+    for (let i = 0; i < availCellsIndexes.length; i++) {
+        const currentTestPlayInfo = {};
+
+        currentTestPlayInfo.index = currBdSt[availCellsIndexes[i]];
+        currBdSt[availCellsIndexes[i]] = currMark;
+
+        const nextMark = aiMark === currMark ? humanMark : aiMark
+        const result = negamax(currBdSt, nextMark);
+        currentTestPlayInfo.score = result.score * -1;
+
+        currBdSt[availCellsIndexes[i]] = currentTestPlayInfo.index;
+        allTestPlayInfos.push(currentTestPlayInfo);
+    }
+
+    for (let i = 0; i < allTestPlayInfos.length; i++) {
+        const score = allTestPlayInfos[i].score;
+        if (score > bestScore) {
+            bestScore = score;
+            bestTestPlay = i;
+        }
+    }
+
+    return allTestPlayInfos[bestTestPlay];
+}
+
+const currentPlayer = aiMark
+const bestPlayInfo = negamax(currentBoardState, currentPlayer);
+console.log(`Best move for ${currentPlayer}: ${bestPlayInfo.score}/${bestPlayInfo.index}`)
